95519e71feef9234688ec43d80ca1b179ee75389
Otavio Corrêa P. Silva <otaviocorrea@gmail.com>
2022-08-24 21:28:37 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_234659) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_002629) do
   create_table "business_contacts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.integer "type"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_234659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_business_contacts_on_email", unique: true
+  end
+
+  create_table "plant_species", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "productive_properties", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
