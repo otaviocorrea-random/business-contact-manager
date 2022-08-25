f52a2127678d7eabb7f728431619e45afa884bda
Otavio Corrêa P. Silva <otaviocorrea@gmail.com>
2022-08-25 01:19:24 -0300

    adjust enumns sintax and add validate to association with businnescontact

--- app/models/productive_property.rb
+++ app/models/productive_property.rb
@@ -1,7 +1,9 @@
 class ProductiveProperty < ApplicationRecord
-  enum :space_between_rows_unit => [:cm, :m]
-  enum :space_between_rows_unit => [:cm, :m]
+  enum :space_between_rows_unit, [:cm, :m], scopes: false
+  enum :space_between_plants_unit, [:cm, :m], scopes: false
 
   belongs_to :business_contact
+  validates_associated :business_contact
+  
   has_and_belongs_to_many :plant_species
 end
