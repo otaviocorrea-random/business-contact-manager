33703d6acdbb59bf6b9593213c2f78c39716bed5
Otavio Corrêa <otaviocorrea@gmail.com>
2022-08-29 21:07:12 -0300

    Add CI to run the tests (#1)
    * Add github ci
    
    * Adjust github ci
    
    * test tuning that was breaking the pipeline

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe "Auths", type: :request do
   describe "GET /login" do
     it "returns http success" do
-      get "/auth/login"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
